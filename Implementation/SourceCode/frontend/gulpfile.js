e67b3a17275b8b79e0a22227487407df83ab7dcf
paulisch <paul.schmutz96@gmail.com>
2017-06-01 14:57:58 +0200

    frontend setup, create gulpfile for development

---
+++ Implementation/SourceCode/frontend/gulpfile.js
@@ -0,0 +1,136 @@
+// gulp
+var gulp = require('gulp'),
+jshint = require('gulp-jshint'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+watch = require('gulp-watch'),
+webserver = require('gulp-webserver'),
+sequence = require('gulp-sequence')
+bower = require('gulp-bower'),
+ngAnnotate = require('gulp-ng-annotate')
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+cleanCSS = require('gulp-clean-css'),
+rename = require('gulp-rename'),
+merge = require('merge-stream'),
+del = require('del');
+
+var paths = {
+  dev: {
+    all: 'app/**',
+    css: 'app/**/*.css',
+	sass: 'app/**/*.scss',
+    index: 'app/index.html',
+    html: 'app/**/*.html',
+    js: 'app/**/*.js',
+    images: 'app/img/**',
+    jsDependencies: [
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js'
+    ]
+  },
+  build: {
+    all: 'dist/**',
+    main: 'dist/',
+    css: 'dist/css',
+    js: 'dist/js',
+    images: 'dist/img'
+  }
+};
+
+// tasks
+gulp.task('webserver', ['styles', 'js', 'html'], function () {
+  gulp.src('dist')
+    .pipe(webserver({
+      //directoryListing: true,
+      //open: true,
+      livereload: true,
+      host: "0.0.0.0"
+    }));
+});
+
+gulp.task('lint', function() {
+  gulp.src(paths.dev.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('clean', function() {
+    return del([paths.build.all]);
+});
+
+gulp.task('styles', function() {
+	var sassStream,
+    cssStream;
+
+	  //compile sass
+	  sassStream = gulp.src(paths.dev.sass)
+		.pipe(sass({style: 'expanded', errLogToConsole: true}));
+
+	  //select additional css files
+	  cssStream = gulp.src(paths.dev.css);
+
+  return merge(sassStream, cssStream)
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(concat('style.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.build.css));
+});
+
+gulp.task('images', function() {
+  gulp.src(paths.dev.images)
+    .pipe(gulp.dest(paths.build.images));
+});
+
+
+gulp.task('js', function() {
+	gulp.src(paths.dev.js)
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest(paths.build.js));
+});
+
+gulp.task('jsDependencies', function () {
+  gulp.src(paths.dev.jsDependencies)
+    .pipe(ngAnnotate()) // needed for angularjs dependencies
+    .pipe(concat('dependencies.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.build.js));
+});
+
+gulp.task('html', function () {
+  gulp.src(paths.dev.html)
+    .pipe(gulp.dest(paths.build.main));
+});
+
+gulp.task('index', function () {
+  gulp.src(paths.dev.index)
+    .pipe(gulp.dest(paths.build.main));
+});
+
+gulp.task('watch', ['styles', 'js', 'html'], function () {
+  //gulp.watch(paths.dev.all, ['styles', 'js', 'html']);
+  
+  gulp.watch([paths.dev.sass, paths.dev.css], ['styles']);
+  gulp.watch(paths.dev.js, ['js']);
+  gulp.watch(paths.dev.html, ['html']);
+  gulp.watch(paths.dev.images, ['images']);
+  gulp.watch(paths.dev.index, ['index']);
+});
+
+
+gulp.task('bower-install', function () {
+  return bower({cmd: 'install'});
+});
+
+gulp.task('bower-update', function () {
+  return bower({cmd: 'update'});
+});
+
+gulp.task('build', sequence('clean', 'bower-install', ['jsDependencies', 'images', 'styles', 'js', 'index', 'html'], 'lint'));
+gulp.task('run', sequence('build', 'webserver', 'watch'));
+gulp.task('default', ['run']);
